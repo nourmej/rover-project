7483cfb5777329473cb0e1d7e6297e381d656c41
nourmej <mejdoub.nour@gmail.com>
2023-06-22 12:57:43 +0200

    improve perf

--- src/main/java/com/example/rovers/App.java
+++ src/main/java/com/example/rovers/App.java
@@ -7,19 +7,29 @@ import java.io.IOException;
 public class App {
 
     public static void main(String[] args){
-    	
+    			
     	 if (args.length < 1) {
-    		
+               System.out.println("input is missing");		
 	            return;
 	        }
 
 	        String inputFile = args[0];
 
 	        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
-	            String plateauSizeLine = reader.readLine();
-	            String[] plateauSize = plateauSizeLine.split(" ");
-	            int plateauWidth = Integer.parseInt(plateauSize[0]);
-	            int plateauHeight = Integer.parseInt(plateauSize[1]);
+	            String line = reader.readLine();
+	            if (line == null || line.isEmpty()) {
+	                System.out.println("Input file is empty.");
+	                return;
+	            }
+	            String[] plateauSize = line.split("\\s+");
+	            
+	            if (plateauSize.length < 2) {
+	                System.out.println("Invalid plateau coordinates.");
+	                return;
+	            }
+	            
+	            int maxX = Integer.parseInt(plateauSize[0]);
+	            int maxY = Integer.parseInt(plateauSize[1]);
 	          
 	            String roverLine;
 	            while ((roverLine = reader.readLine()) != null) {
@@ -28,13 +38,14 @@ public class App {
 	                int y = Integer.parseInt(position[1]);
 	                char heading = position[2].charAt(0);
 
-	                Rovers rover = new Rovers(x, y, heading);
+	                Rovers rover = new Rovers(x, y, heading, maxX, maxY);
 
 	                String instructions = reader.readLine();
 	                rover.processInstructions(instructions);
 	                
 	                System.out.println(rover.getPosition());
 	            }
+	            
 	        } catch (IOException e) {
 	            e.printStackTrace();
 	        }
--- src/main/java/com/example/rovers/Rovers.java
+++ src/main/java/com/example/rovers/Rovers.java
@@ -1,16 +1,20 @@
 package com.example.rovers;
 
-
+import java.util.Arrays;
 
 class Rovers {
-    private int x;
+	private int x;
     private int y;
     private char heading;
+    private int maxX;
+    private int maxY;
 
-    public Rovers(int x, int y, char heading) {
+    public Rovers(int x, int y, char heading, int maxX, int maxY) {
         this.x = x;
         this.y = y;
         this.heading = heading;
+        this.maxX = maxX;
+        this.maxY = maxY;
     }
 
     public void processInstructions(String instructions) {
@@ -45,15 +49,17 @@ class Rovers {
 
     private void move() {
         switch (heading) {
-            case 'N' -> y++;
-            case 'E' -> x++;
-            case 'S' -> y--;
-            case 'W' -> x--;
+        case 'N' -> y = Math.min(y + 1, maxY);
+        case 'E' -> x = Math.min(x + 1, maxX);
+        case 'S' -> y = Math.max(y - 1, 0);
+        case 'W' -> x = Math.max(x - 1, 0);
         }
     }
 
     public String getPosition() {
-        return x + " " + y + " " + heading;
+    	 StringBuilder positionBuilder = new StringBuilder();
+    	 positionBuilder.append(x).append(" ").append(y).append(" ").append(heading);
+    	 return positionBuilder.toString();
     }
 }
 
